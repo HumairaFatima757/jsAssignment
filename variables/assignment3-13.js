e8562e0b6c1147c2a4a301a97b89ef5abe58cab6
HumairaFatima757 <fatimahumaira757@gmail.com>
2023-05-15 23:57:59 +0500

    1st commit

---
+++ variables/assignment3-13.js
@@ -0,0 +1,166 @@
+// var age = 22;
+// alert("i am " + age + "year old ");
+// let times = 0 ;
+
+// alert("you have visited asite "+ times +"times");
+
+// let birthYear = 2001;
+// document.write("My birth year is " + 2001 + "<br>"); 
+// document.write("Data type of my declared variable is number");
+
+// var visitorsName = " Humaima ";
+// var productTitle = " T-shirt ";
+// var quantityOfOrder  = 5 ;
+
+// document.write(visitorsName  + "  ordered "+  quantityOfOrder + productTitle + " on xyz store "   )
+
+
+
+/
+
+// ===7
+// let priceOfitem1 = 650;
+// let quantityOfitem1 = 3;
+// let priceOfitem2 = 100;
+// let quantityOfitem2 = 2;
+// let shippingCharges = 100;
+
+// let totalCost = ( priceOfitem1 * quantityOfitem1)+(priceOfitem2 *  quantityOfitem2);
+
+// document.write ("price Of item1 is " +  priceOfitem1 +"<br>" );
+// document.write("Quantity of item 1 is "+ quantityOfitem1  +"<br>");
+// document.write ("price Of item1 is " +  priceOfitem2  +"<br>");
+// document.write("Quantity of item 2 is "+ quantityOfitem2   +"<br>");
+// document.write("Shipping Charges is " + shippingCharges  +"<br>" +"<br>"  );
+// document.write("Total cost of your Order is " +  totalCost  );
+  
+// ===8
+
+
+
+// let totalMarks = 980;
+// let obtainedMarks  = 804;
+
+// let percentage = (obtainedMarks/totalMarks)*100;
+// document.write("<h1> " +" Total Marks" + "<h1>" +"<br>" );
+// document.write("Total marks:" + totalMarks +"<br>");
+// document.write("Obtained marks:" + obtainedMarks +"<br>");
+// document.write("Percentage:" + percentage + "%");
+
+
+// ==9
+// let us$ = 10;
+// let saudiRiyals = 25;
+
+// let pakrupees =  (us$*104.80 )+ (saudiRiyals*28);
+
+// document.write("Total Currency in PKR is :" +  pakrupees  );
+
+// ===10
+// let num1 =  2;
+// document.write( "Our Rsesult is "+ num1+5) *10/2;  
+
+// ==11
+// let currentYear = 2023;
+// let birthYear = 2001;
+// let age1  = currentYear- birthYear;
+// let age2 = age1-1 ;
+
+// document.write(" They are either  "+ age1 +"or "+ age2);
+
+//  let n = +prompt("Enter any number ");
+//  let n1 = ++n;
+//  let n2 = n++;
+//  let n3 = n--;
+//  let n4 = --n;
+//  document.write("Result:" +"<br>");
+//  document.write("The value of n is: " + n  +"<br>" );
+//  document.write("The value of ++n  is: " + n1  +"<br>");
+//  document.write("Now the value of n is " +  n1 +"<br>" )
+//  document.write("The value of n++  is: " + n2  +"<br>");
+//  document.write("Now the value of n is " +  n2 +"<br>" )
+//  document.write("The value of n--  is: " + n3  +"<br>");
+//  document.write("Now the value of n is " +  n3 +"<br>" )
+//  document.write("The value of --n  is: " + n4 +"<br>");
+//  document.write("Now the value of n is " +  n4 +"<br>" )
+
+
+// FOR ANY TABLE USE THIS 
+
+
+   // let table = +prompt("enter number which you want table ");
+   // if (table !=0){
+      
+         
+   //       for (let i = 1; i <= 10 ; i++){
+   //        document.write( table+ " x " + i + " = " + table *i +"<br>");
+   //       }
+   // }
+   // else{
+   //    let i = 5
+   //    for (let i = 1; i <= 10 ; i++){
+   //     document.write( 5+ " x " + i + " = " + 5 *i + "<br>");
+   //      }
+
+
+   // }
+
+
+
+   // assignment no 9- 11
+
+
+   // let city = +prompt("Enter city name: ");
+   // if (city ==="karachi"){
+   //    document.write ("Welcomee to city of lights")
+   // }
+
+
+// let gender = prompt("enter your gender")
+// if (gender === "female"){
+//    alert("good Morning madam")
+// }
+//  elseif(gender === "male");{
+//    alert("good morning sir ");
+   
+// }
+
+// let fuel = +prompt("enter remaining fuel in car");
+// if (fuel<0.25){
+// document.write("Please refill the fuel in your car");}
+
+// let temp = +pompt ("Enter the temperature");
+// if (temp>40){
+//    document.write("Its toohot outside  ");
+
+// }elseif(temp>30){
+//    document.write("The weather today is normal");
+
+// }elseif(temp>20){
+//    document.write("The weather today is cool");
+   
+// }elseif(temp>10){
+//    document.write("The weather today is so cool");
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
